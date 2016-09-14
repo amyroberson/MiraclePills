f6f08ad2e521723b89c3a6ca2ad0c4335df8586a
Amy Roberson <amy@roberson.xyz>
2016-09-14 18:42:02 -0400

    First Commit

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Amy Roberson on 9/14/16.
+//  Copyright © 2016 Amy Roberson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
